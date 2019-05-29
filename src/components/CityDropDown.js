f3c3984bab069c4b360572f105ac207f79bc5e68
Jon Woo <jw56578@gmail.com>
2019-05-28 20:28:15 -0500

    Update CityDropDown.js

--- src/components/CityDropDown.js
+++ src/components/CityDropDown.js
@@ -11,7 +11,7 @@ function CityDropDown(props) {
         }>
           <option value="Austin">Austin</option>
           <option value="New York">New York</option>
-          <option value="New Olreans">New Olreans</option>
+          <option value="New Olreans">New Orleans</option>
           <option value="Las Vegas">Las Vegas</option>
           <option value="Seattle">Seattle</option>
           <option value="San Fransisco">San Fransisco</option>
